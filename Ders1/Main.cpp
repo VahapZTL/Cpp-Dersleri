3a9b26074cddb215f9349cf3a96ec77ad07f6ee6
VahapZTL <120320108@student.beykent.edu.tr>
2016-12-04 19:53:46 +0300

    İncelenmesi Gereken1

--- Ders1/Main.cpp
+++ Ders1/Main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-	int sayiDizi[5][2];
+	int sayiDizi[2][2], sayiDizi2[2][2], carpim[2][2];
 	
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < 2; i++)
 	{
 		for (int j = 0; j < 2; j++)
 		{
@@ -15,11 +15,34 @@ int main()
 		}
 	}
 
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < 2; i++)
 	{
 		for (int j = 0; j < 2; j++)
 		{
-			cout << "[" << sayiDizi[i][j] << "]";
+			cout << "[" << i << "]" << " " << "[" << j << "]" << "Elemani Girin:\n";
+			cin >> sayiDizi2[i][j];
+		}
+	}
+
+	//Çarpma iþlemi.
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			for (int k = 0; k < 2; k++)
+			{
+				carpim[i][j] += sayiDizi[i][k] * sayiDizi2[k][j];
+			}
+		}
+	}
+
+	cout << "Sonuc: \n";
+	//Sonuç.
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			cout << "[" << carpim[i][j] << "]";
 		}
 		cout << endl;
 	}
