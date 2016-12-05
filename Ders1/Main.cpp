9cb39ade91fe287b3fbf10565efbc0259b0477c5
VahapZTL <120320108@student.beykent.edu.tr>
2016-12-05 17:56:02 +0300

    Fonksiyonlar Dersi

--- Ders1/Main.cpp
+++ Ders1/Main.cpp
@@ -1,60 +1,37 @@
-#include <iostream>
+ï»¿#include <iostream>
 
 using namespace std;
 
+bool asalBul(int a);
+
 int main()
 {
-	int sayiDizi[2][2], sayiDizi2[2][2], carpim[2][2];
-	
-	for (int i = 0; i < 2; i++)
-	{
-		for (int j = 0; j < 2; j++)
-		{
-			cout << "[" << i << "]" << " " << "[" << j << "]" << "Elemani Girin:\n";
-			cin >> sayiDizi[i][j];
-		}
-	}
+	int sayi1 = 0;
+	cout << "Bir sayi Girin\n";
+	cin >> sayi1;
 
-	for (int i = 0; i < 2; i++)
+	if (asalBul(sayi1))
 	{
-		for (int j = 0; j < 2; j++)
-		{
-			cout << "[" << i << "]" << " " << "[" << j << "]" << "Elemani Girin:\n";
-			cin >> sayiDizi2[i][j];
-		}
+		cout << sayi1 << " Asal Sayidir.\n";
 	}
-
-	for (int i = 0; i < 2; i++)
+	else
 	{
-		for (int j = 0; j < 2; j++)
-		{
-			carpim[i][j] = 0;
-		}
-	}
-
-	//Çarpma iþlemi.
-	for (int i = 0; i < 2; i++)
-	{
-		for (int j = 0; j < 2; j++)
-		{
-			for (int k = 0; k < 2; k++)
-			{
-				carpim[i][j] += sayiDizi[i][k] * sayiDizi2[k][j];
-			}
-		}
-	}
-
-	cout << "Sonuc: \n";
-	//Sonuç.
-	for (int i = 0; i < 2; i++)
-	{
-		for (int j = 0; j < 2; j++)
-		{
-			cout << "[" << carpim[i][j] << "]";
-		}
-		cout << endl;
+		cout << sayi1 << " Asal Degildir.\n";
 	}
 
 	system("PAUSE");
 	return 0;
+}
+
+bool asalBul(int a) 
+{
+	for (int i = 2; i < a; i++)
+	{
+		if (a % i == 0)
+		{
+			return false;
+			break;
+		}
+	}
+	return true;
 }
